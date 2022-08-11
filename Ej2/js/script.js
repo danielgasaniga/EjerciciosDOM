f61fae0cee018b3a5d7c0c5e9d3e1b87e3f01fd2
danielgasaniga <daniel.gasaniga@gmail.com>
2022-08-10 21:23:53 -0300

    a

--- Ej2/js/script.js
+++ Ej2/js/script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Escribe el código necesario aquí
-  
+  const highlight = document.getElementById("highlight")
+
+  highlight.addEventListener("click", () => {
+    document.getElementById("lorem").className = "highlight";
+  });
   //
 });
